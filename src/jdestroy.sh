e60f718929513b08d4d16dcc06c7ba4eecede1ad
John Kaul <john.kaul@outlook.com>
2024-10-25 18:26:28 -0500

    jdestroy.sh:
    1.  Made script better at handing deleting of directory and conf file.
        -i.e. does not error out (cancel) if one does not exist so, now
        script will try to verify if item exists and moves on to the next
        item if it does not. This should help in the situation where only
        the userland exists and not the conf file.

--- src/jdestroy.sh
+++ src/jdestroy.sh
@@ -2,20 +2,85 @@
 
 ## Support Functions
 # {{{
-
-PROGNAME=`basename $0`
-# Bail out and exit.
-bailout() {
-    echo "**ERROR** $PROGNAME: $*" >&2
-    exit 1
-}
-
 # Bail out if we don't have root privileges.
 test_root() {
-if [ $(id -u) -ne 0 ]; then
-	bailout "This script must be run as root."
-    fi
+		if [ $(id -u) -ne 0 ]; then
+				bailout "This script must be run as root."
+		fi
 }
+
+# err --
+#   all error messages should be directed to `stderr`.
+# EX
+#   if ! do_something; then
+#       err "unable to do_something"
+#       exit 1
+#   fi
+err() {     #{{{
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] *ERROR*: $*" >&2
+    return 0
+}
+#}}}
+
+# validate --
+#   validate a variable is either a directory or file (and is readable).
+# EX
+#   if ! _validate "$_source" ; then
+#       exit 1
+#   fi
+validate() {        #{{{
+   local _source
+   _source=$1
+
+   if [ -f "$_source" ] || [ -d "$_source" ]; then
+       if [ ! -r "$_source" ]; then
+           err "Source not readable: ${_source}"
+           return 1
+       fi
+#       return 0
+   else
+       err "source not valid: $*"
+       return 1
+   fi
+}
+#}}}
+
+# assert --
+#   Assert a variable is NOT empty.
+# EX
+#   if _assert "$_sourcefile" ; then
+#       do_something
+#   else
+#       err "Missing variable \"_sourcefile\""
+#       exit 1
+#   fi
+assert() {      #{{{
+   local _source
+   _source=$1
+
+   if [ -z $_source ]; then 
+       return 1 
+   fi
+#   return 0
+}
+#}}}
+
+# config_read_file --
+#    Read a file and look for a value.
+config_read_file() {        #{{{
+    (grep -E "${2}=" -m 1 "${1}" 2>/dev/null) | head -n 1 | cut -d '=' -f 2-;
+}
+#}}}
+
+# cofig_get --
+#   A wrapper to call `config_read_file` and set the variable value.
+# EX
+#       config_variable=$(config_get some_variable "${_prefix}/some_config.cfg")
+config_get() {      #{{{
+   val="$(config_read_file "${2}" "${1}")";
+   printf -- "%s" "${val}";
+}
+#}}}
 # }}}
 
 # Environment sanity check
@@ -23,18 +88,19 @@ test_root
 
 jailname=$1
 
-if [ "$(find /etc/jail.conf.d/ -type f -name "${jailname}.conf")" = "" ]; then
-	bailout "**ERROR** Cannot locate \"${jailname}\" configuration script."
+_jcreate_conf=$(find /usr/local/etc/ -type f -name 'jcreate.conf')
+
+_container_path=$(config_get containers.path ${_jcreate_conf})
+if assert ${_container_path} && validate ${_container_path}; then
+		# stop the jail
+		service jail stop ${jailname}
+		# Remove flags.
+		chflags -R 0 ${_container_path}/${jailname}
+		# Delete directory
+		rm -rf ${_container_path}/${jailname}
 fi
 
-# stop the jail
-service jail stop $jailname
-
-# Remove flags.
-chflags -R 0 /usr/local/jails/containers/$jailname
-
-# Delete directory
-rm -rf /usr/local/jails/containers/$jailname
-
-# Delete the .conf file.
-rm /etc/jail.conf.d/${jailname}.conf
+_container_conf=$(config_get containers.conf ${_jcreate_conf})
+if assert ${_container_conf} && validate ${_container_conf}; then
+		rm ${_container_conf}/${jailname}.conf
+fi
